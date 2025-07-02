f00ddcd12973f3826cbb55e3cc902f59ffb2ef44
katherinetsai <katherine.tsai@ideax.biz>
2025-07-02 21:45:41 +0800

    修正

--- src/module/post/post.module.ts
+++ src/module/post/post.module.ts
@@ -4,13 +4,23 @@ import { PostController } from './post.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostEntity, TagEntity } from '@src/common/entities';
 import { PostLogEntity, PostTagLogEntity, PostTagEntity, LikeEntity } from './entities';
+import { CommentService } from '../comment/comment.service';
+import { CommentEntity } from '../comment/entities/comment.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([PostEntity, PostTagEntity, PostLogEntity, TagEntity, PostTagLogEntity, LikeEntity]),
+        TypeOrmModule.forFeature([
+            PostEntity,
+            PostTagEntity,
+            PostLogEntity,
+            TagEntity,
+            PostTagLogEntity,
+            LikeEntity,
+            CommentEntity,
+        ]),
     ],
     controllers: [PostController],
-    providers: [PostService],
+    providers: [PostService, CommentService],
     exports: [PostService],
 })
 export class PostModule {}
