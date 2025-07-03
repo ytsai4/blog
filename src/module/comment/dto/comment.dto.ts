6f3e995f9035c889295cebc049e5c7d5996c4ffd
katherinetsai <katherine.tsai@ideax.biz>
2025-07-03 10:10:00 +0800

    留言Dto

--- src/module/comment/dto/comment.dto.ts
+++ src/module/comment/dto/comment.dto.ts
@@ -1,15 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 
 export class CommentDto {
+    @Expose()
     @ApiProperty({ description: '留言UUID' })
     UUID: string;
-
+    @Expose()
     @ApiProperty({ description: '內文' })
     Content: string;
-
+    @Expose()
     @ApiProperty({ description: '作者 UUID' })
     CreateBy: string;
-
+    @Expose()
     @ApiProperty({ description: '建立時間', type: String, format: 'date-time' })
     CreateDate: Date;
 }
